00613b6d5e3e34938b802fa0986f9e0629d20ea6
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-08-16 08:56:58 -0500

    Ajustes

--- src/views/Tema10.vue
+++ src/views/Tema10.vue
@@ -7,10 +7,79 @@
         span 10
       h1 Normativa seguridad y salud en el Trabajo 
    
-    Separador
+    .fondo1
+      .row.justify-content-center.mb-5(data-aos="fade-right")
+        .col-lg-3.col-10
+          img.mb-lg-0.mb-3(src="@/assets/curso/tema10/img01.png")
+        .col-lg-9.col-12
+          .row.justify-content-center.mb-4
+            .col-lg-4.col-10
+                img.mb-lg-0.mb-3(src="@/assets/curso/tema10/img02.png")
+            .col-lg-8.col-12    
+              .cajon.color-acento-contenido.p-4.mb-3
+                p.mb-0 La implementación de los proyectos productivos pecuarios es una de las actividades que se ha convertido en alternativa de empleo digno para miles de familias en el territorio nacional, ya que es una industria creciente en un país que cada vez demanda más alimentos y materias primas. 
+              p.mb-0 Como muchas otras industrias en Colombia, la empresa pecuaria ha evolucionado en los últimos años y se ha transformado conforme a las exigencias normativas que rigen los temas de empleo, seguridad y formalización laboral. Igualmente, la adopción por parte de muchos empresarios del agro de los lineamientos y principios de las Buenas Prácticas Pecuarias (BPP) y Buenas Prácticas Agrícolas (BPA) ha llevado a que se incorporen en la actividad productiva estos elementos, con lo cual <b style="color:#A3314F">el sector agropecuario ha dado un paso importante hacia la tecnificación de los procesos y el desarrollo de la actividad productiva conforme a los estándares internacionales</b>. 
+          p.mb-4 En cuanto a la normativa relacionada con la seguridad y salud en el trabajo se tiene que desde la década de los 50 se vienen dando avances importantes en lo relacionado con la reivindicación de los derechos de los trabajadores y la búsqueda de relaciones más justas entre estos y sus patronos. Con la incorporación del Código Sustantivo del Trabajo se logró aportar a la consolidación de las relaciones entre trabajadores y empleadores dentro de un ambiente de coordinación económica y equilibrio social. 
+          p.mb-0 A continuación, se presenta un recuento del marco normativo que acompaña los procesos de seguridad y salud en el trabajo. 
+    
+      div.fondoblanco.px-3(style="background-color:white")
+        div.d-flex.justify-content-center.p-0.position-relative
+          img.p-0.m-0.position-relative(style="top:-6px; width: 507px" src="@/assets/curso/tema10/img03.png")
+        .row.justify-content-center
+          .col-lg-10.col-12
+            ul.lista-ul--color
+              li.mb-0
+                i.fas.fa-check
+                | Código Sustantivo del Trabajo. 1951 Regulación de Relaciones Laborales.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 2400 1979 por la cual se establecen algunas disposiciones sobre vivienda, higiene y seguridad en los establecimientos de trabajo.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 2013 1986 por la cual se reglamenta la organización y funcionamiento de los Comités de Medicina, Higiene y Seguridad Industrial en los lugares de trabajo
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 1792 1990 por la cual se adoptan valores límites permisibles para la exposición ocupacional al ruido.
+              li.mb-0
+                i.fas.fa-check
+                | Decreto-Ley 1295 1994 por el cual se determina la organización y administración del Sistema General de Riesgos Profesionales.
+              li.mb-0
+                i.fas.fa-check
+                | Ley 1010 2006 por medio de la cual se adoptan medidas para prevenir, corregir y sancionar el acoso laboral y otros hostigamientos en el marco de las relaciones de trabajo.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 1401 2007 por la cual se reglamenta la investigación de incidentes y accidentes de trabajo.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 2346 2007 por la cual se regula la práctica de evaluaciones médicas ocupacionales y el manejo y contenido de las historias clínicas ocupacionales.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 2646 2008 por la cual se establecen disposiciones y se definen responsabilidades para la identificación, evaluación, prevención, intervención y monitoreo permanente de la exposición a factores de riesgo psicosocial en el trabajo y para la determinación del origen de estas.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 0652 2012 por la cualquier se establece la conformación y funcionamiento del Comité de Convivencia Laboral en entidades públicas y empresas privadas y se di tan otros disposiciones.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 1409 2012 por la cual se establece el reglamento de seguridad para protección contra caídas en trabajo en alturas.
+              li.mb-0
+                i.fas.fa-check
+                | Decreto 1072 2015 Decreto Único Reglamentario del Sector Trabajo.
+              li.mb-0
+                i.fas.fa-check
+                | Resolución 0312 2019 por la cual se definen los Estándares Mínimos del Sistema de Gestión de la Seguridad y Salud en el Trabajo SG-SST.
+    
+      Separador
 
-    #t_10_1.titulo-segundo.color-acento-contenido
-      h2 10.1 Características
+      #t_10_1.titulo-segundo.color-acento-contenido
+        h2 10.1 Características
+
+      .row.justify-content-center.mb-5
+        .col-lg-5.col-12
+          p.mb-lg-0.mb-3 Para hablar de seguridad y salud en el trabajo obligatoriamente se debe hablar de riesgos, pues estos son inherentes a toda actividad humana. De acuerdo con el Ministerio de Trabajo, un riesgo se define <b style="color:#A3314F">como la combinación de la probabilidad de que ocurra una o más exposiciones o eventos peligrosos y la severidad del daño que puede ser causada por estos</b>. Por otro lado, “la estimación del riesgo consiste en emitir un juicio sobre la tolerancia o no del riesgo estimado. En este orden de ideas, la seguridad y salud en el trabajo es el desarrollo de un proceso lógico y por etapas, basado en la mejora continua, con el objetivo de anticipar, reconocer, evaluar y controlar los riesgos que puedan afectar la seguridad y salud en el trabajo” (Ministerio de trabajo 2016).
+        .col-lg-7.col-8
+          img.mb-0(src="@/assets/curso/tema10/img04.png")
+
+    
 
     Separador
 
